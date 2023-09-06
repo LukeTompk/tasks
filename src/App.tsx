8fe92f24b370c4daaf3e957ca07a4921030b30b6
LukeTompk <boxlan246@gmail.com>
2023-09-06 19:35:26 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <h1>Luke Tompkins</h1>
         </div>
     );
 }
